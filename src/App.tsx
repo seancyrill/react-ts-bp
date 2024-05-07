4f00b4cfeb9704dd44b7654e8a0fda1abe6c527c
Sean <seancyrill@gmail.com>
2024-05-08 07:59:04 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+            tesat
+        </div>
+    )
+}
+
+export default App
